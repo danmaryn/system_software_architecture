9d8c99d456b8eafdcaf4fdc274838faf4edc12d1
danmaryn <marynenko.danylo@lll.kpi.ua>
2026-05-28 15:49:57 +0000

    feat: implement task 4.3

---
+++ pz4/task4_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    void *ptr = malloc(0);
+
+    if (ptr == NULL) {
+        printf("malloc(0) returned NULL\n");
+    } else {
+        printf("malloc(0) returned a non-NULL pointer: %p\n", ptr);
+    }
+
+    free(ptr);
+    printf("Memory freed successfully.\n");
+
+    return 0;
+}
